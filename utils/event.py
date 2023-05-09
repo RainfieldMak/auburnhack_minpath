b6f12b39af0663bfeb77248de052212269a57d7a
mrf <mrf01@uab.edu>
2023-05-09 18:43:17 -0500

    add attribute and funciton for half_hour_span

--- utils/event.py
+++ utils/event.py
@@ -6,10 +6,11 @@ class Event:
 
         #place: string , name of place
         #time : tuple, e.g. [star_time , end_time], where start_time and end_time is in string , 24 hour format e.g. 2300
-
+        #half_hour_span: string , total no of half hour between start_time and end_time.
         self.place=place
         self.start_time=time[0]
         self.end_time=time[1]
+        self.half_hour_span= self.calculate_half_hours(self.get_start(), self.get_end_time())
         
 
 
@@ -22,14 +23,24 @@ class Event:
     def get_end_time(self):
         return self.end_time
     
+    def get_half_hour_span(self):
+        return self.half_hour_span
+    
     def to_string(self):
-        return self.get_place() +" "+ self.get_start()+" " + self.get_end_time()
+        return self.get_place() +" "+ self.get_start()+" " + self.get_end_time() + " " + self.get_half_hour_span()
     
 
-
+    #caculate how many half hour in between start and end time
+    #return : string
+    def calculate_half_hours(self,start_time_str, end_time_str):
+        start_time = int(start_time_str[:2]) * 60 + int(start_time_str[2:])
+        end_time = int(end_time_str[:2]) * 60 + int(end_time_str[2:])
+        delta_minutes = end_time - start_time
+        half_hours = delta_minutes / 30
+        return str(int(half_hours))
 
     
 if __name__ == "__main__":
 
-    test1=Event("uab", ["0000", "0100"])
+    test1=Event("uab", ["0000", "1000"])
     print(test1.to_string())
